1e5473db9445bdd3b528aa791234bd05c7a4d77f
calvinnolimit <115436085+calvinnolimit@users.noreply.github.com>
2023-01-19 13:00:39 +0700

    first commit

---
+++ resources/views/livewire/berita.blade.php
@@ -0,0 +1,48 @@
+<div>
+    <div class="container">
+     <div class="row">
+        <div class="col-3">
+        <div class="card">
+  <div class="card-header">
+    Status
+  </div>
+  <div class="card-body">
+
+  komentar <br />
+  <input type="text" class="form-control" wire:model='txtkomentar' />
+  <br />
+  <input type="submit" class="btn btn-primary" value="KIRIM" wire:click='simpan' />
+  </div>
+</div>
+    </div>
+    <div class="col-9">
+    <div class="card">
+  <div class="card-header">
+    Kirim saya
+  </div>
+  <div class="card-body">
+    Saya :<br />
+    <p>
+      <i>
+        {{ $txtkomentar }}
+</i>
+</p>
+<hr />
+@foreach ($isi_Komentar as $item)
+  Saya pada {{ Carbon\Carbon::parse($item->created_at)->isoFormat('dddd, D MMMM Y') }}
+  menulis:<br />
+  <p>
+    <i>
+      {{ $item->isi_komentar}}
+</i>
+<button class="float-end btn btn-danger" wire:click="hapus('{{ $item->id }}')">
+  hapus
+</button>
+<hr />
+@endforeach
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+   
+</head>
+
+<body>
+<div class="container">
+<nav class="navbar navbar-light bg-primary">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Navbar</span>
+  </div>
+</nav>
+</div>
+         
+    @livewire('berita')
+
+    @livewireScripts
+</body>
+
+</html>
+
+</head>
+<body>
+    
+</body>
+</html>
